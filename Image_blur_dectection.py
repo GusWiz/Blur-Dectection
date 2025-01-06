27a3220ddad12f74d2650709b83bb429c8cb329e
Gustavo H <packy512@gmail.com>
2025-01-05 19:54:02 -0600

    Added Comments to is_blurry function

--- Image_blur_dectection.py
+++ Image_blur_dectection.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 # Funciton that detects if the image is blurry by using the Laplacian variance
-def is_blurry(image_path):
+def is_blurry(image_path, threshold = 100):
   img = cv2.imread(image_path)
   # Check worse case - No image is found
   if img is None:
@@ -11,14 +11,15 @@ def is_blurry(image_path):
 
   # Convert the image to grayscale
   gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+  # The code above is only necessary if we do NOT change the imread() parameter to read image 
+  # with cv2.IMREAD_GRAYSCALE
   # Apply the Laplacian operator to detect for edges
   # the less amount of edges indicate a blurry image
   laplacian_var = cv2.Laplacian(gray_image, cv2.CV_64F).var()
 
   # If the variance is below threshould (is smooth and lacks edges), it is considered blurry
-  if laplacian_var > 120: # Threshould can be adjusted if need be
+  if laplacian_var < threshold: # Threshould can be adjusted if need be
     return True
   return False
-
+  # You can replace last 3 lines with return laplacian_var < threshold
 
